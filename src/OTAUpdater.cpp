538e81ce7ee110b863263794618c6b3bb6960409
eatrisno <eaprilitrisno@gmail.com>
2026-09-14 01:36:17 +0900

    Fix premature OTA blacklist on download failure

--- src/OTAUpdater.cpp
+++ src/OTAUpdater.cpp
@@ -101,12 +101,12 @@ void OTAUpdater::checkForUpdate() {
                 
                 if (bin_url != "") {
                     Serial.println("[OTA] Ditemukan file .bin! Mengunduh dari: " + bin_url);
-                    // Kita akan menandai failed_tag SEBELUM update. 
-                    // Jika update sukses dan berhasil boot, kita akan hapus failed_tag ini di begin() firmware baru.
-                    // Tapi karena firmware baru yang punya begin(), firmware lama hanya bisa set flag.
-                    _prefs.putString("failed_tag", latest_tag); 
                     
                     if (performUpdate(bin_url.c_str(), latest_tag.c_str())) {
+                        // Kita akan menandai failed_tag SETELAH download sukses tapi SEBELUM reboot. 
+                        // Jika firmware baru gagal boot (crash), dia akan rollback dan blacklist ini tetap ada.
+                        // Jika sukses boot, firmware baru akan menghapus blacklist ini di begin().
+                        _prefs.putString("failed_tag", latest_tag);
                         Serial.println("[OTA] Update selesai! Restarting...");
                         delay(1000);
                         ESP.restart();
