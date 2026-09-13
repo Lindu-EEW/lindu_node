e794fb8b83266a6f63e4064af46140bb31b4ec0d
eatrisno <eaprilitrisno@gmail.com>
2026-09-13 21:15:17 +0900

    Enhance I2C probe to read Chip ID, preventing phantom ACK deadlocks on disconnected sensors

--- src/SensorManager.cpp
+++ src/SensorManager.cpp
@@ -1,8 +1,21 @@
 #include "SensorManager.h"
 
 bool probeI2C(uint8_t address) {
+  Wire1.setTimeOut(50); // Set timeout hanya untuk I2C Cuaca
   Wire1.beginTransmission(address);
-  return (Wire1.endTransmission() == 0);
+  if (Wire1.endTransmission() == 0) {
+    // Validasi ekstra: Coba baca 1 byte register ID (0xD0 untuk BMP/BME280)
+    Wire1.beginTransmission(address);
+    Wire1.write(0xD0);
+    Wire1.endTransmission();
+    if (Wire1.requestFrom(address, (uint8_t)1) == 1) {
+      uint8_t id = Wire1.read();
+      if (id == 0x58 || id == 0x60 || id == 0x56 || id == 0x57) { // ID valid BMP/BME
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 void SensorManager::begin(QueueHandle_t queue) {
