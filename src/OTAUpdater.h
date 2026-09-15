6a45cb345c7295b47f6d982fdb8f664a67e7415c
eatrisno <eaprilitrisno@gmail.com>
2026-09-16 01:32:25 +0900

    feat: add LED color animations during OTA update (blinking orange while downloading, solid purple when successful)

--- src/OTAUpdater.h
+++ src/OTAUpdater.h
@@ -11,7 +11,7 @@
 #include "esp_system.h"
 
 #ifndef CURRENT_VERSION
-#define CURRENT_VERSION "v1.2.5"
+#define CURRENT_VERSION "v1.2.6"
 #endif
 
 class OTAUpdater {
--- src/main.cpp
+++ src/main.cpp
@@ -188,7 +188,15 @@ void networkTaskCode(void* parameter) {
                 } else {
                     pixels.setPixelColor(0, pixels.Color(0, 0, 0));
                     digitalWrite(BUZZER_PIN, HIGH);
-                }} else if (!sensorMgr.sensor_ok && !hw611_ok) {
+                }
+            } else if (otaUpdater.ota_status == "DOWNLOADING_FIRMWARE" || otaUpdater.ota_status == "CHECKING_GITHUB") {
+                // OTA SEDANG MENGUNDUH: Berkedip Kuning/Oranye sangat cepat layaknya loading
+                if ((millis() / 80) % 2 == 0) pixels.setPixelColor(0, pixels.Color(255, 120, 0));
+                else pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+            } else if (otaUpdater.ota_status == "UPDATE_SUCCESS_RESTARTING") {
+                // OTA SELESAI & SIAP RESTART: Menyala Ungu Solid
+                pixels.setPixelColor(0, pixels.Color(128, 0, 128));
+            } else if (!sensorMgr.sensor_ok && !hw611_ok) {
                 
                 // Semua sensor mati: Berkedip Merah Cepat (Bahaya Fatal)
                 if ((millis() / 200) % 2 == 0) pixels.setPixelColor(0, pixels.Color(50, 0, 0));
